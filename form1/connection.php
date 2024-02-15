e29177f98b20b4904330f7c01af9560249226b42
Faros <99401477+Faros998@users.noreply.github.com>
2024-02-15 13:05:00 +0100

    Add files via upload

---
+++ form1/connection.php
@@ -0,0 +1,8 @@
+<?php
+// Nastavení údajů pro připojení k databázi
+$host = 'sql4.webzdarma.cz'; // Název hostitele databáze
+$dbname = 'faroswzcz6369'; // Název databáze
+$username = 'faroswzcz6369'; // Uživatelské jméno databáze
+$password = 'Passw1rd+'; // Heslo k databázi
+
+?>
